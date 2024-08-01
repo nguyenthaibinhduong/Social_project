619657858deb33fd0c18d7c78c661ab767bcc37f
nguyenthaibinhduong <natteam1998@gmail.com>
2024-08-01 22:23:00 +0700

    v-1

--- client/src/components/leftbar/leftbar.js
+++ client/src/components/leftbar/leftbar.js
@@ -1,10 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import { Link } from "react-router-dom";
+
 function Leftbar() {
+    const { currentUser,logout } = useContext(AuthContext);
+    const { isPending, isError, data:user} = useQuery({
+        queryKey: ['currentuser'],
+        queryFn: () => makeRequest.get('/users/find/' + currentUser.id ).then(res => {
+            return res.data;
+        }),
+    });
     return ( 
         <div className="card">
            <ul className="nav flex-column">
-                <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="#">Active</a>
-                </li>
                 <li className="nav-item">
                     <a className="nav-link" href="#">Home</a>
                 </li>
@@ -15,7 +25,6 @@ function Leftbar() {
                     <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
-            <br/>
             <ul className="nav flex-column">
                 <li className="nav-item">
                     <a className="nav-link" aria-current="page" href="#">Active</a>
